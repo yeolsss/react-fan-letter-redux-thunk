af5b34ba8aff8cdac78d392c0b0549a11b21eca0
yeolsss <yeol8810@gmail.com>
2023-11-30 17:35:31 +0900

    1차 완성

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -10,17 +10,28 @@ import {
   StWriterInfoWrapper,
 } from '../styles/detail/StDetail.js';
 import { validData } from '../common/util';
-import {
-  deleteLetter,
-  updateLetter,
-} from '../redux/config/module/letter.slice.js';
 import { setCurrentMember } from '../redux/config/module/member.slice.js';
 import { printError } from '../redux/config/module/error.slice.js';
 import api from '../axios/jsonServer.api.js';
+import { printSuccess } from '../redux/config/module/success.slice.js';
+import { selectorLoginData } from '../redux/config/module/login.slice.js';
 
 function Detail() {
-  const { id: parmaId, memberId } = useParams();
+  const { userInstance } = useSelector(selectorLoginData);
+  const { id: paramId, memberId } = useParams();
   const dispatch = useDispatch();
+
+  const [letter, setLetter] = useState({
+    id: '',
+    writedTo: '',
+    nickname: '',
+    content: '',
+    createdAt: '',
+    avatar: '',
+  });
+
+  const { id, writedTo, nickname, content, createdAt, avatar, userId } = letter;
+
   useEffect(() => {
     dispatch(setCurrentMember(memberId));
   }, []);
@@ -31,34 +42,67 @@ function Detail() {
   const navigate = useNavigate();
 
   // * letter update textarea state
-  const [letterUpdateContent, setLetterUpdateContent] = useState('');
+  const [letterUpdateContent, setLetterUpdateContent] = useState();
+
   // * letter update btn state
   const [updateState, setUpdateState] = useState(false);
 
   const updateLetterContentRef = useRef(null);
 
-  const handleOnSubmitDeleteLetter = (e) => {
-    const { id } = e.target.dataset;
-    dispatch(deleteLetter(id));
-    alert('삭제되었습니다.');
-    navigate('/');
+  const member = members.getMembers[Number(writedTo)];
+  const { name } = member;
+
+  const handleOnSubmitDeleteLetter = async (e) => {
+    const confirmResult = confirm('삭제 하시겠습니까?');
+    if (!confirmResult) return;
+
+    try {
+      await api.delete(`/letters/${paramId}`);
+      dispatch(
+        printError({
+          isError: true,
+          errorMessage: '삭제되었습니다.',
+        }),
+      );
+      navigate('/');
+    } catch (error) {
+      dispatch(
+        printError({
+          isError: true,
+          errorMessage: '삭제가 실패했습니다.',
+        }),
+      );
+    }
   };
   // letter 수정
   const handleOnChangeUpdateLetter = (e) => {
     setLetterUpdateContent(e.target.value);
   };
 
-  const handleOnSubmitUpdateLetter = (e) => {
+  const handleOnSubmitUpdateLetter = async (e) => {
     e.preventDefault();
     if (validData(letterUpdateContent, '내용', updateLetterContentRef)) return;
 
-    const { id } = e.target.dataset;
-    dispatch(
-      updateLetter({
-        id,
-        content: letterUpdateContent.replaceAll('\n', '<br>'),
-      }),
-    );
+    try {
+      const response = api.patch(`/letters/${paramId}`, {
+        content: letterUpdateContent,
+      });
+      setLetter({ ...letter, content: letterUpdateContent });
+      dispatch(
+        printSuccess({
+          isSuccess: true,
+          successMessage: '글 수정이 완료되었습니다.',
+        }),
+      );
+    } catch (error) {
+      dispatch(
+        printError({
+          isError: true,
+          errorMessage: '수정에 실패하였습니다.',
+        }),
+      );
+    }
+
     setUpdateState(false);
   };
 
@@ -67,28 +111,27 @@ function Detail() {
     setUpdateState(true);
   };
 
-  /*  useEffect(() => {
-    setLetterUpdateContent(content);
-  }, []);*/
-
   // json server data
-  useEffect(async () => {
-    try {
-      const response = await api.get(`/letters?id=${parmaId}`);
-      console.log(response.data);
-    } catch (error) {
-      dispatch(
-        printError({
-          isError: true,
-          errorMessage: error,
-        }),
-      );
-    }
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await api.get(`/letters?id=${paramId}`);
+        setLetterUpdateContent(response.data[0].content);
+        setLetter({ ...response.data[0] });
+      } catch (error) {
+        dispatch(
+          printError({
+            isError: true,
+            errorMessage: error,
+          }),
+        );
+      }
+    })();
   }, []);
 
   return (
     <StDetailContainer>
-      {/*<div>
+      <div>
         <StWriterInfoWrapper>
           <div>
             <Avatar imgPath={avatar} />
@@ -113,25 +156,26 @@ function Detail() {
             <p>{content.replaceAll('<br>', '\n')}</p>
           )}
         </StContentForm>
-        <StButtonWrapper>
-          {updateState ? (
+        {userInstance.userId === userId ? (
+          <StButtonWrapper>
+            {updateState ? (
+              <Button
+                dataId={id}
+                handler={handleOnSubmitUpdateLetter}
+                btnTitle={'완료'}
+              />
+            ) : (
+              <Button btnTitle={'수정'} handler={handleOnClickUpdateBtn} />
+            )}
+
             <Button
               dataId={id}
-              handler={handleOnSubmitUpdateLetter}
-              btnTitle={'완료'}
+              btnTitle={'삭제'}
+              handler={handleOnSubmitDeleteLetter}
             />
-          ) : (
-            <Button btnTitle={'수정'} handler={handleOnClickUpdateBtn} />
-          )}
-
-          <Button
-            dataId={id}
-            btnTitle={'삭제'}
-            handler={handleOnSubmitDeleteLetter}
-          />
-        </StButtonWrapper>
-      </div>*/}
-      test
+          </StButtonWrapper>
+        ) : null}
+      </div>
     </StDetailContainer>
   );
 }
